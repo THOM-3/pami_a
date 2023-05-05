d6892727cbef411832c5f97828b05fa0d4624f8f
THOM-3 <71737896+THOM-3@users.noreply.github.com>
2023-05-04 21:24:26 -0300

    04-05

---
+++ components/apostila_03/index.js
@@ -0,0 +1,15 @@
+
+import { View, Text } from 'react-native';
+
+import styles from './styles'
+
+export default function App() {
+    return(
+        <View style={styles.container} >
+            <apostila03/>
+            <StatusBar style="auto" />
+                Exemplo 3
+        </View>
+    );
+}
+
---
+++ components/apostila_03/styles.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#f5f5f5',
+        padding: 8,
+
+    },
+ });
+        export default styles;
