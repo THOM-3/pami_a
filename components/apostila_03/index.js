f3cdf5e706ae5ea003630e2a0b4cb7b2c4c0bc67
THOM-3 <71737896+THOM-3@users.noreply.github.com>
2023-05-11 20:48:38 -0300

    AULA-1105

--- components/apostila_03/index.js
+++ components/apostila_03/index.js
@@ -1,16 +1,31 @@
+import react, { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
 import styles from './styles'
 
 export default function apostila03() {
+
+const [contador, setContador] = useState(0);
+
+function addNumber() {
+  setContador(contador + 1);
+}
+
+function addNumber() {
+  setContador(contador - 1);
+}
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>Exemplo apostila 3</Text>
+
+      <Text style={styles.textCounter}>{contador}</Text>
+
       <TouchableOpacity
        style={styles.button}
-        onPress={alert('salve')}
+        onPress={() => addNumber()}
          >
-        <Text style={styles.textButton}> Olá Mundo!!! </Text>
+        <Text style={styles.textButton} >+</Text>
+        <Text style={styles.textButton} >-</Text>
       </TouchableOpacity>
     </View>
   );
