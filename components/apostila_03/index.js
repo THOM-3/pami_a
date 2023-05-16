1501ba8e0f602fa7582f78bbfb44a88e50374104
THOM-3 <71737896+THOM-3@users.noreply.github.com>
2023-05-16 20:08:13 -0300

    16-05

--- components/apostila_03/index.js
+++ components/apostila_03/index.js
@@ -11,9 +11,14 @@ function addNumber() {
   setContador(contador + 1);
 }
 
-function addNumber() {
+function subNumber() {
   setContador(contador - 1);
 }
+
+function zerarNumber() {
+  setContador(contador (0));
+}
+
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>Exemplo apostila 3</Text>
@@ -23,6 +28,22 @@ function addNumber() {
       <TouchableOpacity
        style={styles.button}
         onPress={() => addNumber()}
+         >
+        <Text style={styles.textButton} >+</Text>
+        <Text style={styles.textButton} >-</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+       style={styles.button}
+        onPress={() => subNumber()}
+         >
+        <Text style={styles.textButton} >+</Text>
+        <Text style={styles.textButton} >-</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+       style={styles.button}
+        onPress={() => zerarNumber()}
          >
         <Text style={styles.textButton} >+</Text>
         <Text style={styles.textButton} >-</Text>
